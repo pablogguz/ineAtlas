659ee01ee214782d74d6e906350262aa058d8ce4
pablogguz <72149928+pablogguz@users.noreply.github.com>
2024-11-02 13:41:48 +0100

    update

--- data-raw/1. process_ine_data.r
+++ data-raw/1. process_ine_data.r
@@ -500,12 +500,3 @@ process_ine_data <- function(indicator_type) {
     tract = section_data_wide
   )))
 }
-
-# Run function ----
-# process_ine_data("income")
-# process_ine_data("income_sources")
-# process_ine_data("demographics")
-# process_ine_data("distribution_sex")
-
-process_ine_data("distribution_sex_age")
-# process_ine_data("distribution_sex_nationality")
---
+++ data-raw/2. run_processing.r
@@ -0,0 +1,7 @@
+# Run function ----
+process_ine_data("income")
+process_ine_data("income_sources")
+process_ine_data("demographics")
+process_ine_data("distribution_sex")
+process_ine_data("distribution_sex_age")
+process_ine_data("distribution_sex_nationality")
